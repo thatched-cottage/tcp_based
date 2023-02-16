cc833785e513a6d54b48fdbcdaee9fa935adacae
”wuyang“ <yang.wu@centurygame.com>
2023-02-16 13:23:46 +0800

    feat: merige

--- pkg/server.go
+++ pkg/server.go
@@ -4,8 +4,4 @@ import (
 	"github.com/thatched-cottage/tcp_based.git/pkg/base_info"
 )
 
-<<<<<<< HEAD
-var Server_name base_info.ServerNode
-=======
 var ServerName base_info.ServerNode
->>>>>>> c43ea45 (feat: 开始优化)
