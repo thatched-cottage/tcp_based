352d34f0d18e48db67810f0e03a3a497641cfff6
”wuyang“ <yang.wu@centurygame.com>
2023-02-16 13:22:07 +0800

    feat: 开始优化

--- example/node/node.go
+++ example/node/node.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"flag"
+<<<<<<< HEAD
 	"gitee.com/wuxiansheng/tcp_based.git/pkg"
 	"gitee.com/wuxiansheng/tcp_based.git/pkg/ININP"
 	"gitee.com/wuxiansheng/tcp_based.git/pkg/base_info"
 	"gitee.com/wuxiansheng/tcp_based.git/pkg/log"
 	_ "gitee.com/wuxiansheng/tcp_based.git/pkg/node"
 )
+=======
+	"github.com/thatched-cottage/tcp_based.git/pkg"
+	"github.com/thatched-cottage/tcp_based.git/pkg/ININP"
+	"github.com/thatched-cottage/tcp_based.git/pkg/base_info"
+	"github.com/thatched-cottage/tcp_based.git/pkg/log"
+	_ "github.com/thatched-cottage/tcp_based.git/pkg/node"
+)
+
+>>>>>>> c43ea45 (feat: 开始优化)
 var nodename = flag.Int("nodename", 0, "node name")
 
 func main() {
 	flag.Parse()
+<<<<<<< HEAD
 	pkg.Server_name = base_info.ServerNode(*nodename)
 	log.Debugf("这是%s", base_info.NodeName[pkg.Server_name])
+=======
+	pkg.ServerName = base_info.ServerNode(*nodename)
+	log.Debugf("这是%s", base_info.NodeName[pkg.ServerName])
+>>>>>>> c43ea45 (feat: 开始优化)
 	ININP.IINP.Run(":8888")
 }
--- pkg/server.go
+++ pkg/server.go
@@ -4,4 +4,8 @@ import (
 	"github.com/thatched-cottage/tcp_based.git/pkg/base_info"
 )
 
+<<<<<<< HEAD
 var Server_name base_info.ServerNode
+=======
+var ServerName base_info.ServerNode
+>>>>>>> c43ea45 (feat: 开始优化)
